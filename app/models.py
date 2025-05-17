09ad02c011c634ae2c0e247b35c31a7c64c15369
sehee0207 <lolry0207@gmail.com>
2025-05-17 21:49:37 +0900

    마지막~

--- app/models.py
+++ app/models.py
@@ -4,3 +4,20 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    tag_set = models.ManyToManyField("Tag", blank=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    message = models.TextField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
